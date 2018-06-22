9c11869170119d4f1377fad014ce0c40f650a47f
LukeLss <2804220037@qq.com>
2018-06-23 01:57:51 +0800

    update logic

--- src/main/java/com/lss/sample/GameStrategy2.java
+++ src/main/java/com/lss/sample/GameStrategy2.java
@@ -7,20 +7,30 @@ package com.lss.sample;
 public class GameStrategy2 implements ThreeAndFiveGame{
 
     public void print(int num) {
+        int single = num % 10;
+        int ten = num / 10;
         /**
-         * num is divisible by 3 or 3 is in num
+         * num is divisible by 3
          */
-        boolean three = (num % 3 == 0) || (num % 10 == 3);
+        boolean three = (num % 3 == 0) ;
+        /**
+         * 3 is in num
+         */
+        boolean inThree = ((ten == 3) || (single == 3));
         /**
          * num is divisible by 5 or 5 is in num
          */
-        boolean five = (num % 5 == 0) || (num % 10 == 5);
+        boolean five = (num % 5 == 0) ;
+        /**
+         * 5  is in num
+         */
+        boolean inFive = ((ten == 5) || (single == 5));
 
-        if (three && five) {
+        if ((three || inThree) && (five || inFive)) {
             System.out.println("FizzBuzz");
-        } else if (three) {
+        } else if (three || inThree) {
             System.out.println("Fizz");
-        } else if (five) {
+        } else if (five || inFive) {
             System.out.println("Buzz");
         } else {
             System.out.println(num);
